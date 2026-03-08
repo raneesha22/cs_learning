580a8be5bba1693014e848efab8b78c0b6177b35
Raneesha Ranasinghe <raneesharanasinghe@gmail.com>
2026-03-08 14:36:52 +0530

    chore : FizzBuzz

--- learning.py
+++ learning.py
@@ -9,3 +9,16 @@ def reverse_string(s):
 
 #to test
 print(reverse_string("Hello"))
+
+#FizzBuzz
+#Print numbers 1 to 100. If divisible by 3 → print "Fizz". If by 5 → "Buzz". If by both → "FizzBuzz". Otherwise print the number.
+for i in range(1,101):
+    if i%15=0:
+        print("FizzBuzz")
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
+        
