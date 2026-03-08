fef96767671ed0815f65f690214f6543710c8607
Raneesha Ranasinghe <raneesharanasinghe@gmail.com>
2026-03-08 14:03:33 +0530

    chore : reverse string

--- learning.py
+++ learning.py
@@ -1,1 +1,11 @@
-print("python learning.py")
+#Reverse a String 
+#Write a function that takes a string like "hello" and returns it reversed: "olleh". Don't use a built-in reverse method.
+def reverse_string(s):
+    result=""
+
+    for i in range(len(s)-1,-1,-1):
+        result += s[i]
+    return result
+
+#to test
+print(reverse_string("Hello"))
