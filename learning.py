d69672483878f01949103d9faf38b4f6dd240b2e
Raneesha Ranasinghe <raneesharanasinghe@gmail.com>
2026-03-15 23:22:49 +0530

    chore : fibnoacci series

--- learning.py
+++ learning.py
@@ -68,3 +68,20 @@ def two_sum(nums,target):
         seen[num]=i
 
     return[]
+
+#Fibonacci Sequence
+
+def fibonacci(n):
+    if n ==0:
+        return 0
+    if n == 1:
+        return 1
+    
+    a=0
+    b=1
+
+    for _ in range (2,n+1):
+        a=b
+        b=a+b
+
+        return b
